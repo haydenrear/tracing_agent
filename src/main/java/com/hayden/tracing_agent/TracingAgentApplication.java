6920dc57f6f52a5efe236c00febfa25f21d724f6
hayden.rear <hayden.rear@gmail.com>
2024-02-26 19:28:58 -0500

    updated

---
+++ src/main/java/com/hayden/tracing_agent/TracingAgentApplication.java
@@ -0,0 +1,13 @@
+package com.hayden.tracing_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracingAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TracingAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hayden/tracing_agent/TracingAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hayden.tracing_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracingAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
